587ff466d2c482484bb96e0913579a5d3885ec73
Jeff Fang <sanlyfang@gmail.com>
2014-09-20 23:59:35 +1000

    Refactor config classes

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -2,44 +2,13 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
-
-    @Configuration
-    public static class WebMvcConfig extends WebMvcConfigurerAdapter {
-        @Override
-        public void addViewControllers(ViewControllerRegistry registry) {
-            registry.addViewController("/signin");
-        }
-    }
-
-    @Configuration
-    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
-                    .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    .loginPage("/signin")
-                    .permitAll()
-                    .and()
-                .logout()
-                    .permitAll();
-        }
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ src/main/java/demo/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package demo.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/signin")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+}

5944b1adacdbc0417cd7b642d2eb553f4cf82214
Jeff Fang <sanlyfang@gmail.com>
2014-09-21 18:53:17 +1000

    Database support

--- src/main/java/demo/config/SecurityConfig.java
+++ src/main/java/demo/config/SecurityConfig.java
@@ -3,12 +3,18 @@ package demo.config;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // TODO:
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/demo/web/SocialController.java
+++ src/main/java/demo/web/SocialController.java
@@ -1,4 +1,4 @@
-package demo;
+package demo.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

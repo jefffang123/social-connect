5d77af6fdddd361b6e4520d1d50e461ca260298d
Jeff Fang <sanlyfang@gmail.com>
2014-09-24 20:27:24 +1000

    Form authentication based on database

--- src/main/java/demo/config/SecurityConfig.java
+++ src/main/java/demo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -7,14 +8,21 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
-        // TODO:
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, true from user where username = ?")
+                .authoritiesByUsernameQuery("select username, 'ROLE_USER' from user where username = ?");
     }
 
     @Override

d640fb9c904ccfd7884820b487b3b41358bff2e7
Jeff Fang <sanlyfang@gmail.com>
2014-09-27 00:33:30 +1000

    Minor Refactoring

--- src/main/java/demo/web/UserController.java
+++ src/main/java/demo/web/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class SocialController {
+public class UserController {
 
     @RequestMapping("/")
     public String home() {

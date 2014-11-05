d6b05139b6e62aa4617809f5c257ab4c4223b34a
Jeff Fang <sanlyfang@gmail.com>
2014-11-05 20:46:57 +1100

    Refactored UserTests

--- src/test/java/demo/UserTests.java
+++ src/test/java/demo/UserTests.java
@@ -8,13 +8,17 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.Filter;
 
+import static demo.SecurityRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -38,8 +42,50 @@ public class UserTests {
     }
 
     @Test
-    public void homeNotLoggedIn() throws Exception {
+    public void homeRequiresLogin() throws Exception {
         mvc.perform(get("/"))
-                .andExpect(redirectedUrl("http://localhost/signin"));
+                .andExpect(result -> {
+                    status().isMovedTemporarily().match(result);
+                    redirectedUrl("http://localhost/signin").match(result);
+                });
+    }
+
+    @Test
+    public void loginWithInvalidUsernamePassword() throws Exception {
+        RequestBuilder request = post("/signin")
+                .param("username", "jeff")
+                .param("password", "invalid")
+                .with(csrf());
+
+        mvc.perform(request)
+                .andExpect(result -> {
+                    status().isMovedTemporarily().match(result);
+                    redirectedUrl("/signin?error").match(result);
+                });
+    }
+
+    @Test
+    public void loginNotAllowedForDisabledUser() throws Exception {
+        RequestBuilder request = post("/signin")
+                .param("username", "test")
+                .param("password", "test1234")
+                .with(csrf());
+
+        mvc.perform(request)
+                .andExpect(result -> {
+                    status().isMovedTemporarily().match(result);
+                    redirectedUrl("/signin?error").match(result);
+                });
+    }
+
+    @Test
+    public void loginWithValidUsernamePassword() throws Exception {
+        RequestBuilder request = post("/signin")
+                .param("username", "jeff")
+                .param("password", "test1234")
+                .with(csrf());
+
+        mvc.perform(request)
+                .andExpect(redirectedUrl("/"));
     }
 }

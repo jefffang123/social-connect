c968db369984e109715774ab0c7f1110020ae3dc
Jeff Fang <sanlyfang@gmail.com>
2014-11-11 23:28:47 +1100

    View controller for homepage

--- src/main/java/demo/web/UserController.java
+++ src/main/java/demo/web/UserController.java
@@ -3,7 +3,6 @@ package demo.web;
 import demo.domain.User;
 import demo.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,11 +13,6 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
-
-    @RequestMapping("/")
-    public ModelAndView home(@AuthenticationPrincipal User user) {
-        return new ModelAndView("home", "user", user);
-    }
 
     @RequestMapping(value = "/signup", method = RequestMethod.GET)
     public ModelAndView signup() {
